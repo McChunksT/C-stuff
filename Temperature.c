90f67317ed9fc0877a7ef9e60a4cae322392cbf9
Celena <42121844+candreal@users.noreply.github.com>
2020-07-17 23:57:15 -0500

    Proposed Style Changes

--- Temperature.c
+++ Temperature.c
@@ -2,17 +2,18 @@
 
 int main(void) 
 {
-  //create int for celsius
-  float C;
-  //create int for fahrenheit
-  float F;
+  //create float variables for celsius and fahrenheit
+  float C, F;
+  
   //prompt user to enter temp in celsius
   printf("Enter Temperature in C: ");
+  
+  //store user input in variable C
   scanf("%f", &C);
+  
   //take given int and convert to fahrenheit
-  F = (C * 9/5) + 32; 
+  F = (C * 9/5) + 32;
+  
   //print result
   printf("%.2f\n", F);
-
-
 }
